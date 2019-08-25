e14c4bef2e974b515fdc8fae765001445e31461a
alexz1124 <alexz.waranyu@gmail.com>
2019-08-25 18:04:46 +0700

    Task: Add conditional navigation

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -19,7 +19,7 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
-                R.layout.fragment_title,container,false)
+                R.layout.fragment_title, container, false)
         binding.playButton.setOnClickListener { view ->
             view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
 
